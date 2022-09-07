c56ac57fe508b976ef649a65b4b8e09cab27abb2
castellanos94 <aca810@gmail.com>
2022-09-06 22:25:37 -0500

    Remove task from result

--- jfuzzy-logic-core/src/main/java/com/castellanos94/jfuzzylogic/core/base/Result.java
+++ jfuzzy-logic-core/src/main/java/com/castellanos94/jfuzzylogic/core/base/Result.java
@@ -1,12 +1,9 @@
 package com.castellanos94.jfuzzylogic.core.base;
 
-import com.castellanos94.jfuzzylogic.core.task.Task;
-
 import lombok.Data;
 
 @Data
 public abstract class Result {
-    protected Task task;
     protected long startTime;
     protected long endTime;    
 }
